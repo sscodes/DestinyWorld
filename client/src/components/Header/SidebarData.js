8b9039eb6f6f24dc2c961b76d177494490787858
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-05-11 12:39:39 +0530

    Update SidebarData.js

--- client/src/components/Header/SidebarData.js
+++ client/src/components/Header/SidebarData.js
@@ -2,34 +2,41 @@ import React from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
+import * as IcoMoon from 'react-icons/im';
+import * as GameIcons from "react-icons/gi";
 
 export const SidebarData = [
   {
     title: 'Home',
+    body: 'Dapp Homepage',
     path: '/',
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text'
   },
   {
     title: 'Register',
+    body: 'Register into the smart contract',
     path: '/register',
     icon: <IoIcons.IoIosPaper />,
     cName: 'nav-text'
   },
   {
     title: 'Bid',
+    body: 'Bid on the daily minted tokens',
     path: '/bid',
-    icon: <FaIcons.FaLocationArrow />,
+    icon: <IcoMoon.ImHammer2 />,
     cName: 'nav-text'
   },
   {
     title: 'Stake',
+    body: 'Stake tokens in the smart contract',
     path: '/stake',
-    icon: <FaIcons.FaLocationArrow />,
+    icon: <GameIcons.GiReceiveMoney />,
     cName: 'nav-text'
   },
   {
     title: 'FAQ',
+    body: 'Frequently Asked Questions',
     path: '/faq',
     icon: <FaIcons.FaRegQuestionCircle />,
     cName: 'nav-text'
