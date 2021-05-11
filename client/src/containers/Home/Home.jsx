e0ba7a5ef8c5de0b34206bf1288d3ccad0145d97
SANKET SARKAR <47353707+sscodes@users.noreply.github.com>
2021-05-11 20:39:45 +0530

    Update Home.jsx

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
             <Header />
             <Container className="cardCont">
                 <Row>
-                    <Col md={6} className="d-flex justify-content-end">
+                    <Col md={6} className="d-md-flex justify-content-end my-4">
                         <Cards title={"BID ECOIN"} body={"Bid your ECOIN on the daily share of minted DECO tokens. At 00:00 GMT, the minted tokens of the day are distributed based on all the received bids of the day. The highest bidder receives the maximum share of tokens."} />
                     </Col>
-                    <Col md={6} className="d-flex justify-content-start">
+                    <Col md={6} className="d-md-flex justify-content-start my-4">
                         <Cards title={"STAKE DECO"} body={"Stake your DECO tokens to earn reward in form of ECOIN tokens. 95% of the received ECOIN in forms of bidding is distributed among the stakers. Also Staking earns DECO tokens in interest at a 5% APY. Penalty will be charged for withdrawing stake before maturity."} />
                     </Col>
                 </Row>
